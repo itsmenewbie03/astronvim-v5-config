c69b91c192368854647ae861549b9f9988ab312f
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2025-03-29 10:10:32 +0800

    feat: add more personal configs

--- lua/polish.lua
+++ lua/polish.lua
@@ -3,14 +3,20 @@
 -- fit in the normal config locations above can go here
 
 -- Set up custom filetypes
-vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
-  },
-}
+-- vim.filetype.add {
+--   extension = {
+--     foo = "fooscript",
+--   },
+--   filename = {
+--     ["Foofile"] = "fooscript",
+--   },
+--   pattern = {
+--     ["~/%.config/foo/.*"] = "fooscript",
+--   },
+-- }
+
+vim.o.scrolloff = 10
+-- INFO: i wanna keep `gr` for go to references
+vim.api.nvim_set_keymap("n", "gr", " lR", { nowait = true, desc = "Search references" })
+-- INFO: using mouse while coding is gay
+vim.o.mouse = ""
