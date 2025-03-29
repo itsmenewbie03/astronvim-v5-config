581421af02306f2fb50ae23ff42a497ab1adddee
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2025-03-29 09:53:57 +0800

    batman

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,16 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  opts = {
+    -- configuration options...
+    transparent_background = true,
+    flavour = "mocha",
+    -- custom_highlights = {
+    --   -- WARN: who tf added a fucking gray line on my TabLine?
+    --   TabLineFill = { bg = "#1e1e2e" },
+    -- },
+    integrations = {
+      neotree = false,
+    },
+  },
+}
